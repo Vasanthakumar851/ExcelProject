e6ed06b6926cee80eb03f1dc92642ec741df3567
Vasanthakumar851 <vasanthvijay890@gmail.com>
2026-01-21 12:44:28 +0530

    new commits

---
+++ src/test/java/com/excel/ReadValue.java
@@ -0,0 +1,36 @@
+package com.excel;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ReadValue {
+	
+	public static void main(String[] args)throws Throwable {
+		File file=new File(System.getProperty("user.dir")+"\\src\\test\\resources\\ExcelDatas\\sample.xlsx");
+		FileInputStream stream=new FileInputStream(file);
+		Workbook workbook=new XSSFWorkbook(stream);
+		Sheet sheet = workbook.getSheet("Sheet1");
+		int rowCount = sheet.getPhysicalNumberOfRows();
+		
+		for(int i=0;i<rowCount;i++) {
+			System.out.println();
+			Row row = sheet.getRow(i);
+			int cellCount = row.getPhysicalNumberOfCells();
+			for(int j=0;j<cellCount;j++) {
+				Cell cell = row.getCell(j);
+				System.out.print(cell+"\t");
+				
+			}
+		}
+		stream.close();
+		workbook.close();
+		
+	}
+
+}
